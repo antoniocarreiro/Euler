41a04687416819af46c14de7ecdecbe0acdb46db
antoniocarreiro <antoniocarreiro@users.noreply.github.com>
2017-11-19 12:58:20 +0000

    Create prob003.py

---
+++ prob003.py
@@ -0,0 +1,22 @@
+import math
+
+primeFactor = 600851475143
+counter = 3
+sqrtOf = math.sqrt(primeFactor)
+highestValue = 0
+
+def CheckPrime(numCheck):
+    i = 2
+    while i < numCheck:
+        if numCheck % i == 0:
+            return 0
+        i += 1
+    return 1
+
+while counter < sqrtOf:
+    if (primeFactor % counter == 0):
+        if CheckPrime(counter):
+            highestValue = counter
+    counter += 2
+    
+print "The highest prime factor of %d is %d" % (primeFactor, highestValue)
