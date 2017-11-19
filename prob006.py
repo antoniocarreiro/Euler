97e35155e4aeeab2a22e67c6390ba7863f69e154
antoniocarreiro <antoniocarreiro@users.noreply.github.com>
2017-11-19 13:09:32 +0000

    Create prob006.py

---
+++ prob006.py
@@ -0,0 +1,13 @@
+def sum_quare_powers(n):
+    sum = 0
+    for i in range(1, n + 1):
+        sum += i**2
+    return sum
+   
+def square_powers_sum(n):
+    sum = 0
+    for i in range(1, n + 1):
+        sum += i
+    pow = sum**2
+    return pow
+print(square_powers_sum(100) - sum_quare_powers(100))
